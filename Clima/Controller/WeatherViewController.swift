e81ac741ae00a85a82b4bd495a2302146bdbde7a
23ji <isj4194@naver.com>
2025-04-08 17:51:38 +0900

    UITextFieldDelegate 기능 구현

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -18,15 +18,31 @@ class WeatherViewController: UIViewController {
   @IBOutlet weak var searchTextField: UITextField!
   
   
+  // MARK: - Life Cycle
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     self.searchTextField.delegate = self
   }
-  
-  
 }
 
 extension WeatherViewController: UITextFieldDelegate {
+  // 리턴키 눌렸을 때 값 들어오는거 확인
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    guard self.searchTextField.text != "" else { return false }
+    return true
+  }
   
+  // 사용자 입력이 끝났을 때
+  func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+    guard self.searchTextField.text != "" else { return false }
+    return true
+  }
+  
+  // 입력이 완료되었을 때
+  func textFieldDidEndEditing(_ textField: UITextField) {
+    guard let cityName = self.searchTextField.text else { return }
+    self.searchTextField.text = ""
+  }
 }
 
