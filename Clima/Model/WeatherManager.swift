4605779adb38a4e24f0eca09b5c31da85d9eeef5
23ji <isj4194@naver.com>
2025-04-10 17:44:47 +0900

    WeatherManager 생성

---
+++ Clima/Model/WeatherManager.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by 이상지 on 4/10/25.
+//  Copyright © 2025 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherManager {
+  //1. String -> URL
+  //2. URLSession
+  //3. session -> dataTask
+  //4. dataTask.resume()
+  
+  var weatherURL = "https://api.openweathermap.org/data/3.0/onecall?appid=ab2d2e496ebfc6758d71dd007225e58c"
+  //&lat={lat}&lon={lon}&exclude={part}
+  
+}
