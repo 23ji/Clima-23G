983dc18c74ac5e5577e28d78388c81c630bd4b72
23ji <isj4194@naver.com>
2025-04-11 19:51:58 +0900

    ViewController 구성

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,42 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
-
-    @IBOutlet weak var conditionImageView: UIImageView!
-    @IBOutlet weak var temperatureLabel: UILabel!
-    @IBOutlet weak var cityLabel: UILabel!
+class WeatherViewController: UIViewController, UITextFieldDelegate {
+  
+  @IBOutlet weak var conditionImageView: UIImageView!
+  @IBOutlet weak var temperatureLabel: UILabel!
+  @IBOutlet weak var cityLabel: UILabel!
+  @IBOutlet weak var searchWeather: UITextField!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.searchWeather.delegate = self
+  }
+  
+  @IBAction func searchButton(_ sender: Any) {
+    //endediting
+    self.searchWeather.endEditing(true)
+  }
+  
+  //사용자가 입력을 끝낼 때
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    guard textField.text != "" else { return false }
+    //엔터키 눌러도 동작하게
+    return true
+  }
+  
+  //입력이 끝났을 때
+  func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+    guard textField.text != "" else { return false }
+    return true
+  }
+  
+  //리턴키 눌렀을 때
+  func textFieldDidEndEditing(_ textField: UITextField) {
+    //텍스트 값 전달
+    guard let text = textField.text else { return }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+  }
+  
 }
 
